58e05963b91f99643ed6ee4897e0cac435bad9d9
Ameer Masoud <ameer.masoud@gmail.com>
2024-04-23 17:41:41 -0500

    initing

---
+++ card.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+
+struct Card {
+    
+};
---
+++ game.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <cardGroup.h>
+
+class game {
+public:
+
+private:
+    std::vector<std::vector<cardGroup>> cardField;
+};
